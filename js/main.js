b2c138c3c27207a4d865878f0405ac2c9f4f326d
Giaki22 <giaki22@hotmail.com>
2022-05-09 20:36:38 +0200

    + Working Logic, but broken "IF" True statement

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
+let difficulty = difficultyChoice();
+console.log(difficulty);
 let mines = [];
-do {
-    let temp = 0;
-    temp = randomNumber();
-    if (!(mines.includes(temp))){
-        mines.push(temp);
-    }
-} while (mines.length < 16);
+mines = getMines(mines, difficulty);
 console.log(mines);
 
-
-let isUserExploded = false;
 let userNumbers = [];
 let userNumber = 0;
-do{
-    do{
-        userNumber = prompt("Inserisci un numero tra 1 e 100");
-    } while ((isNaN(userNumber) || userNumber < 1 || userNumber > 100) && (isUserExploded == false));
-    if ((mines.includes(userNumber))){
-        alert("BOOM!");
+let isUserExploded = false;
+do {
+    do {
+        userNumber = prompt("Inserisci un numero");
+    } while (isNaN(userNumber) || userNumbers.includes(userNumber));
+    if (mines.includes(userNumber)){
+        alert("BOOOOOOOOOOM!");
         isUserExploded = true;
     }
-    userNumbers.push(userNumber)
-    console.log(userNumbers);
+    userNumbers.push(userNumber);
     console.log(isUserExploded);
-} while (isUserExploded == false && userNumbers.length < (100 - 98))
+    console.log(userNumbers);
+} while (userNumbers.length < (difficulty - 16) && isUserExploded == false);
+alert(`Gioco Finito! Il tuo punteggio: ${userNumbers.length}`);
 
 /* FUNCTIONS */
-function randomNumber(){
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    return randomNumber;
- };
+function getMines(array, max){
+    while (array.length < 16) {
+        const randomNumber = Math.floor(Math.random() * max) + 1;
+        if (!(array.includes(randomNumber))){
+            array.push(randomNumber);
+        }
+    };
+    return array;
+};
+function difficultyChoice(){
+    let level = 0;
+    do {
+        level = prompt("Seleziona difficoltà: 1 -> Facile; 2 -> Intermedio; 3 -> Difficile");
+    } while (isNaN(level) || (!(level == 1) && !(level == 2) && !(level == 3)));
+    if (level == 1){
+        return 100;
+    } else if (level == 2){
+        return 80;
+    } else if (level == 3){
+        return 50;
+    }
+}
